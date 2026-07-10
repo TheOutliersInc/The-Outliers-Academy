fd5b7bac73bf49ca755d3425e30a5373d55c7a81
Vineeetheanalyst <arogundaderidwan38@gmail.com>
2026-07-10 22:33:08 +0100

    ridwan

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ menu.addEventListener("click",()=>{
 nav.classList.toggle("active");
 
 });
+
+const form = document.getElementById("registrationForm");
+
+form.addEventListener("submit", async function(e) {
+
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    await fetch("https://script.google.com/macros/s/AKfycbxg31SALHy5UBKrvgI52__Q0M6ZY6ou5tu8KLv4G20SCQeUgNoTz_bIiLUmeCkyUQzK8Q/exec", {
+
+        method: "POST",
+
+        body: formData
+
+    });
+
+    window.location.href = "pages/thank-you.html";
+
+});
